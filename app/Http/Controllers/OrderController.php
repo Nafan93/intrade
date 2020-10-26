88c40fd986e149012ad5d0908f267d59cdd06444
s <infantyev48@gmail.com>
2020-10-26 11:29:18 +0300

    Fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Config;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderCreated;
+use App\Mail\NewOrder;
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Api;
+
 use Illuminate\Http\Request;
 
 use App\Product;
@@ -18,6 +25,27 @@ class OrderController extends Controller
     public function show($id)
     {
         $order = Order::all()->where('id', $id)->first();
+
+
+        $adminEmail = config('app.adminEmail');
+        $chatId = config('app.chatId');
+        $massage = [
+            'chat_id' => $chatId,
+            'text' => '
+            #новаязаявка #запрос #коммерческоепредложение
+На сайте ' . config('app.url') . ' появилась новая заявка на ' . $order->product->name . '
+
+Имя клиента: ' . $order->name . '
+Номер телефона: ' . $order->phone . '
+Электронная почта: ' . $order->email
+        ];
+
+        if($order->product === null) {
+            Mail::to($order->email)->send(new OrderCreated($order));
+        } else {
+            Mail::to(config('app.adminEmail'))->send(new NewOrder($order));
+        }
+        Telegram::sendMessage($massage);
 
         return view ('admin.orders.show')->with(['order' => $order,
                             'products' => Product::get()
